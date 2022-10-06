d2394e696bb4e0cfeee408be9d05d9c6f41f4743
Jean-François Senécal <jeanjeansenecal@gmail.com>
2022-10-06 14:01:42 -0400

    fix(variables): removed unused variables

--- src/components/Images.js
+++ src/components/Images.js
@@ -3,14 +3,8 @@ import Carousel from 'react-material-ui-carousel';
 // import AECSP from '../Images/AECSP.svg';
 import CGI from '../Images/CGI.png';
 import DESJARDINS from '../Images/desjardins.png';
-import DIGITAL_OCEAN from '../Images/digital-ocean.png';
 import INTAC_LAB from '../Images/IntactLab.png';
 import SEMLA from '../Images/semla.png';
-import SE4AI from '../Images/se4aiB.png';
-import GIRO from '../Images/giro.png';
-import DRUIDE from '../Images/druide.png';
-import GURU from '../Images/guru.png';
-import RCGT from '../Images/rcgt.png';
 
 const sponsorsList = [
   { label: 'SEMLA', image: SEMLA, backgroundColor: '#f9f9f9' },
