db1c680c00d38ad319f2c56247d96bea01365a4f
Jean-François Senécal <jeanjeansenecal@gmail.com>
2022-10-06 13:59:02 -0400

    feat(sponsors): took out lower than gold sponsors from slide show

--- src/components/Images.js
+++ src/components/Images.js
@@ -21,21 +21,11 @@ const sponsorsList = [
   },
   { label: 'CGI', image: CGI, backgroundColor: '#e31937' },
   { label: 'Intact Lab', image: INTAC_LAB, backgroundColor: '#f9f9f9' },
-  {
-    label: 'raymond chabot grant thornton',
-    image: RCGT,
-    backgroundColor: '#f9f9f9',
-  },
-  { label: 'Digital Ocean', image: DIGITAL_OCEAN, backgroundColor: '#0080ff' },
-  { label: 'SE4AI', image: SE4AI, backgroundColor: '#303846' },
-  { label: 'Giro', image: GIRO, backgroundColor: '#ffffff' },
-  { label: 'druide', image: DRUIDE, backgroundColor: '#ffffff' },
-  { label: 'guru', image: GURU, backgroundColor: '#ffffff' },
 ];
 
 const Images = () => {
   return (
-    <Carousel interval='4000' duration='2000'>
+    <Carousel interval='5000' duration='2000'>
       {sponsorsList.map((sponsor, index) => (
         <div
           id='cover-screen'
